ce2520a812dcc39ec64a15a65dc42d461aae2f5e
Unalterable <diamond.oliver@gmail.com>
2016-08-09 09:55:21 +0100

    cleaning tests

--- spec/views/admin/events/show.html.erb_spec.rb
+++ spec/views/admin/events/show.html.erb_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
 
+require 'rails_helper'
 RSpec.describe "admin/events/show", type: :view do
   before(:each) do
     @admin_event = assign(:admin_event, Admin::Event.create!())
