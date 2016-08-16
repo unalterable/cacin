a89efe15ca4f7501c370e643874e8f8ebe254480
Unalterable <diamond.oliver@gmail.com>
2016-08-16 15:33:40 +0100

    invitations_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815150840) do
+ActiveRecord::Schema.define(version: 20160816143312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20160815150840) do
     t.text     "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer  "event_id"
+    t.text     "html"
+    t.text     "plain_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_invitations_on_event_id", using: :btree
   end
 
   create_table "members", force: :cascade do |t|
@@ -53,4 +62,5 @@ ActiveRecord::Schema.define(version: 20160815150840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "invitations", "events"
 end
