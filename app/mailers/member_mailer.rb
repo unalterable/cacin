a8f2474169ac40379d14c2278bf65b66a882ebe2
Unalterable <diamond.oliver@gmail.com>
2016-08-27 21:19:44 +0100

    typos

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -11,10 +11,10 @@ class MemberMailer < ApplicationMailer
   def token_link(token)
     email(  to: token.member.email,
             subject: "CACIN: request to alter details",
-            html: "You (or someone claiming to have your email address) have reqested to alter your CACIN member details.
+            html: "You (or someone claiming to have your email address) have requested to alter your CACIN member details.
             <br>
             Please click <a href='#{rsvp_url(token: token.token)}'>here</a> to edit your details",
-            text: "You (or someone claiming to have your email address) have reqested to alter your CACIN member details.
+            text: "You (or someone claiming to have your email address) have requested to alter your CACIN member details.
             To edit your details please go here: #{rsvp_url(token: token.token)}" )
   end
 
