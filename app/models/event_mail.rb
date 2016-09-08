600815f1f1443a2b68d8dd395dda212644fd35c9
Unalterable <diamond.oliver@gmail.com>
2016-09-08 16:43:31 +0100

    mail sampling

--- app/models/event_mail.rb
+++ app/models/event_mail.rb
@@ -9,7 +9,21 @@ class EventMail < ApplicationRecord
     Mustache.render(plain_text_template, template_vars(args))
   end
 
+  def html_sample
+    render_html(sample_vars)
+  end
+
+  def plain_text_sample
+      render_plain_text(sample_vars)
+  end
+
   private
+
+    def sample_vars
+      { location: event.location,
+        token: "********SAMPLE TOKEN**********",
+        rsvp_url: "http://google.com/?q=sample"}
+    end
 
     def template_vars(args)
       { location: event.location,
